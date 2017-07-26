dd8babc736f87ff8e60e18ce16bf5f35ca105441
Tom Ridge <tom.j.ridge@googlemail.com>
2017-07-26 12:34:29 +0100

    +

---
+++ src/tjr_substring.ml
@@ -0,0 +1,67 @@
+(* This file provides common support for substring/span parsing *)
+
+(* simple span-based substrings ----------------------------------- *)
+
+(* we don't use this type directly *)
+type 'a poly_substring = {
+  input:'a;
+  i_:int;
+  j_:int
+}
+
+let length x = x.j_ - x.i_
+
+(* example terminal parsers ----------------------------------------- *)
+
+type substring_ = string poly_substring
+
+let mk_substring s = {input=s;i_=0;j_=String.length s}
+
+type string_position = {
+  s_:string;
+  i_:int
+}
+
+(* returns the end indexes *)
+type terminal_parser = string_position -> int list
+
+let eps s = [s.i_]
+
+
+let re ~re s = 
+  if (Str.string_match re s.s_ s.i_) then
+    [Str.match_end ()]  (* FIXME return all results? *)
+  else []
+
+let upto_re ~re s =
+  try 
+    Str.search_forward re s.s_ s.i_ |> fun k ->
+    [k]
+  with Not_found -> []
+
+let a lit s = re ~re:Str.(quote lit |> regexp) s
+
+let upto_a lit s = upto_re ~re:Str.(quote lit |> regexp) s
+
+(* sequencing *)
+let ( **> ) p1 p2 = 
+  fun s -> 
+  p1 s 
+  |> List.map (fun i_ -> p2 {s with i_})
+  |> List.concat
+
+
+
+(* example ---------------------------------------------------------- *)
+
+
+module X = functor (Y:sig end) -> struct
+
+let _ = upto_a ")" {s_="...)";i_=0}
+
+let bracket = (a "(") **> (upto_a ")" **> (a ")"))
+
+let _ = bracket
+
+let _ = bracket {s_="(xxx)";i_=0 }
+end
