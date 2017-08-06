28c8043936637970440e0c8625fab0f7a8aca72e
Tom Ridge <tom.j.ridge@googlemail.com>
2017-08-06 16:51:28 +0100

    +

--- src/bash_env.sh
+++ src/bash_env.sh
@@ -2,7 +2,7 @@ set -a # export all vars
 #set -x # debug
 
 libname="tjr_lib"
-required_packages="str,bos.setup,omd,fileutils,yojson,core_kernel,ppx_deriving_yojson,ppx_sexp_conv" #,astring ,ppx_string_interpolate
+required_packages="str,bos.setup,omd,fileutils,yojson,core_kernel,ppx_deriving_yojson,ppx_sexp_conv,core" #,astring ,ppx_string_interpolate
 description="Various useful OCaml functions"
 
 source bash_env.common
