31be01ab7c227f021560b661dfa783fff168303a
Tom Ridge <tom.j.ridge@googlemail.com>
2017-09-27 08:39:53 +0100

    +

--- src/tjr_monad.ml
+++ src/tjr_monad.ml
@@ -29,3 +29,31 @@ end
 module X_ : M_ = M
 
 include M
+
+
+(* generic version parameterized over the basic primitive operations *)
+module Alternative = struct
+
+  type (_,'prim,'e) m = 
+      Return : 'a -> ('a,'prim,'e) m 
+    | Bind:  ('a,'prim,'e) m * ('a -> ('b,'prim,'e) m) -> ('b,'prim,'e) m
+    | Err: 'e -> ('a,'prim,'e) m
+    | Prim: 'prim -> ('a,'prim,'e) m
+
+  let ( >>=) = fun m f -> Bind(m,f)
+
+  let return x = Return x
+
+  let err e = Err e
+
+
+  let example = 
+    let read () = Prim (`Read ()) in
+    let write (s:string) = Prim(`Write s) in
+    read () >>= fun s ->
+    write s >>= fun () -> 
+    return ()
+
+  let _ = example
+
+end
