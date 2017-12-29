b0f687a41d777f9291f7cefb8eda31526b132bce
Tom Ridge <tom.j.ridge@googlemail.com>
2017-12-29 16:32:15 +0000

    +

--- src/bash_env.sh
+++ src/bash_env.sh
@@ -5,19 +5,62 @@ libname="tjr_lib"
 required_packages="str,bos.setup,omd,fileutils,yojson,core_kernel,ppx_deriving_yojson,ppx_sexp_conv,core,lwt" #,astring ,ppx_string_interpolate
 description="Various useful OCaml functions"
 
-source bash_env.common
-
 function mk_links() {
-true
+    for f in core/*.ml non_core/*.ml; do ln -sf $f .; done
 #    for f in msg2/*.ml; do ln -sf $f .; done
 }
 
 
 function clean() {
 	rm -f *.cmi *.cmo *.cmx *.o *.x *.a *.cma *.cmxa
-	find . -xtype l -exec rm -f \{\} \;
+	find . -maxdepth 1 -type l -exec rm -f \{\} \;
 }
 
-function real_clean() {
-	find . -maxdepth 1 -type l -exec rm -f \{\} \;
+
+
+# generic from here ----------------------------------------------------
+
+# was in bash_env.common
+
+PKGS="-package $required_packages"
+SYNTAX=""
+
+# 8~"pattern-matching is not exhaustive"; 
+# 11~"this match case is unused";
+# 26~"unused variable s2"
+WARN="-w @f@p@u@s@40-8-11-26"
+
+# -thread needed for core
+  ocamlc="$DISABLE_BYTE ocamlfind ocamlc   -thread -g $WARN $PKGS $SYNTAX"
+ocamlopt="$DISABLE_NTVE ocamlfind ocamlopt -thread -g $WARN $PKGS $SYNTAX"
+ocamldep="ocamlfind ocamldep $PKGS"
+
+mk_cma="$DISABLE_BYTE ocamlfind ocamlc"
+mk_cmxa="$DISABLE_NTVE ocamlfind ocamlopt"
+
+mls=`ocamldep -sort -one-line *.ml`
+cmos="${mls//.ml/.cmo}"
+cmxs="${mls//.ml/.cmx}"
+
+natives="
+"
+
+branch=`git symbolic-ref --short HEAD` 
+v=`date +'%F'`
+if [ "$branch" = "master" ]; then
+    package_name="${libname}"
+else 
+    package_name="${libname}_${branch}"
+fi
+
+
+function mk_meta() {
+cat >META <<EOF
+name="$package_name"
+description="$description"
+version="$v"
+requires="$required_packages"
+archive(byte)="$libname.cma"
+archive(native)="$libname.cmxa"
+EOF
 }
