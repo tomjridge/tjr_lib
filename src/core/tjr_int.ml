7797a03144efd541d4865b64bbe3e205445633af
Tom Ridge <tom.j.ridge@googlemail.com>
2018-07-03 17:24:26 +0100

    +

--- src/core/tjr_int.ml
+++ src/core/tjr_int.ml
@@ -1,8 +1,10 @@
-module Make_type_isomorphic_to_int() : sig
+module type TYPE_ISOMORPHIC_TO_INT = sig 
   type t
   val int2t: int -> t
   val t2int: t -> int
-end = struct
+end
+
+module Make_type_isomorphic_to_int() : TYPE_ISOMORPHIC_TO_INT = struct
   type t = int
   let int2t i = i
   let t2int i = i
