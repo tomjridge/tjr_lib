d73108ece831b69ff2773de0551305ff0f36e47a
Tom Ridge <tom.j.ridge@googlemail.com>
2018-11-23 14:37:34 +0000

    +

---
+++ src/non_core/tjr_config.ml
@@ -0,0 +1,14 @@
+(** A simple config file functor *)
+
+module Make(S:sig 
+    type config [@@deriving yojson]
+val filename: string
+end) = struct
+  open S
+
+  (** Read config from file *)
+  let config = 
+    Yojson.Safe.from_file filename |> config_of_yojson |> function
+    | Ok r -> r | _ -> failwith __LOC__
+end
+  
