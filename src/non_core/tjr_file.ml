b12a9383cdd1e04f3ccf3abcea53aedd15348394
Tom Ridge <tom.j.ridge@googlemail.com>
2019-01-23 15:45:04 +0000

    +

--- src/non_core/tjr_file.ml
+++ src/non_core/tjr_file.ml
@@ -104,6 +104,14 @@ let cwd () = Unix.getcwd
 (* pread from ExtUnixAll *)
 
 
+(** Get a file descriptor corresponding to a file. Possibly create the
+   file, and init it (truncate it to 0). FIXME move elsewhere? *)
+let fd_from_file ~fn ~create ~init = Unix.(
+    let flgs = [O_RDWR] @ (if create then [O_CREAT] else []) in
+    openfile fn flgs 0o640 |> fun fd -> 
+    (if init then ftruncate fd 0 else ()) |> fun _ -> 
+    fd)
+
 
 (* permissions ------------------------------------------------------ *)
 
