19762fa3e1c920868f6620f958554f099b99170e
Tom Ridge <tom.j.ridge@googlemail.com>
2019-08-01 17:05:13 +0100

    +

--- src/core/tjr_store.ml
+++ src/core/tjr_store.ml
@@ -125,6 +125,8 @@ module With_sig : sig
   val mk_ref : 'a -> fstore -> 'a ref_ * fstore
   val set : 'a ref_ -> 'a -> fstore -> fstore
   val get : 'a ref_ -> fstore -> 'a
+
+  (** an uninitialized reference (no initial value) *)
   val mk_uref : name:string -> fstore -> 'a ref_ * fstore
   val uref_initialized : 'a ref_ -> fstore -> bool
   val urefs_status : fstore -> (string * bool) list
