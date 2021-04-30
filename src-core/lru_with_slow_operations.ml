2a7a5b1f67bfde1653fb68dc6310f1dc3e6f05b4
Tom Ridge <tom.j.ridge@googlemail.com>
2021-04-30 10:47:12 +0100

    Further work on lru_with_slow_operations
    replaces tjr_lru library which is much more complicated

--- src-core/lru_with_slow_operations.ml
+++ src-core/lru_with_slow_operations.ml
@@ -113,47 +113,58 @@ module Make(S:S) = struct
       lru_capacity = cap
     }
 
-  (* FIXME *)
-  let maybe_trim () = return ()
-
-  let rec find_opt c k = 
-    (* try to find in cache *)
-    Lru.find k c.lru |> function
-    | None -> begin
-        (* not in cache, so check whether we are syncing *)
-        match c.is_syncing with
-        | true -> 
-          c.syncing >>= fun () -> 
-          find_opt c k (* try again after sync completes *)
-        | false -> 
-          (* create the promise *)
-          let p = 
-            bot_ops.find_opt bot k >>= fun v -> 
-            begin match v with 
-            | None -> 
-              Lru.add k (Plain (lower_none ())) c.lru
-            | Some v -> 
-              Lru.add k (Plain (lower_some v)) c.lru
-            end;
-            return v
-          in
-          Lru.add k (Finding p) c.lru;
-          maybe_trim () >>= fun () -> 
-          p                   
-      end
-    | Some (Plain x) -> 
-      let (v,_) = x in
-      return v
-    | Some (Finding p) -> 
-      p      
-      
-  let insert c k v = 
-    Lru.add k (Plain (insert v)) c.lru;
-    maybe_trim ()
-
-  let delete c k = 
-    Lru.add k (Plain (delete ())) c.lru;
-    maybe_trim ()
+  (** This is called when the capacity may potentially be exceeded; in
+     this case, we may need to trim some entries by flushing them to
+     the bottom layer. *)
+  let maybe_trim c = 
+    let cap = c.lru_capacity in
+    let sz = Lru.size c.lru in
+    let no_trim = c.is_syncing || sz <= cap in
+    match no_trim with 
+    | true -> return () (* sync automatically trims *)
+    | false -> 
+      (* we want to issue a call to the lower layer, and prevent any
+         concurrent operation overtaking this one - effectively we
+         want to serialize calls to the lower layer; the point is that
+         just by calling the lower layer, we don't guarantee that the
+         promise resolves immediately - it may be delayed and some
+         other operation occurs in the meantime, which may use a newer
+         kv, leading to the newer kv being overwritten by the older;
+      *)
+      (* NOTE: we can drop "finding" entries if we choose *)
+      let to_trim = ref [] in
+      for i = 1 to c.trim_delta do
+        let x = Lru.lru c.lru in
+        Lru.drop_lru c.lru;
+        assert(x <> None); (* we must have elts to drop *)
+        let op = Option.get x in
+        let op = 
+          match op with 
+          | (k,Plain (Some v,dirty)) -> (
+              match !dirty with
+              | true -> `Insert(k,v)
+              | false -> `None)
+          | (k,Plain (None,dirty)) -> (
+              match !dirty with
+              | true -> `Delete k
+              | false -> `None)
+          | (k,Finding _) -> `None 
+          (* we may drop a finding; but all extant computations
+             waiting on the resolved value will still wait of course
+             - we are not deleting the promise! *)
+        in
+        (match op with 
+         | `None -> ()
+         | `Insert (k,v) -> to_trim := `Insert(k,v)::!to_trim
+         | `Delete k -> to_trim := `Delete(k)::!to_trim)
+      done;
+      (* we expect that sync should be true here - no point delaying
+         writing to disk *)
+      let p = bot_ops.exec bot ~sync:true !to_trim in
+      c.is_syncing <- true;
+      p >>= fun () -> 
+      c.is_syncing <- false;
+      return ()
 
   let rec sync c = 
     match c.is_syncing with 
@@ -177,6 +188,8 @@ module Make(S:S) = struct
                 in
                 dirties:=op::!dirties)
               | false -> ());
+        (* take the opportunity to trim some entries *)
+        Lru.trim c.lru;
         (* now process dirties as a batch *)
         bot_ops.exec bot ~sync:true (!dirties)        
       in
@@ -185,9 +198,55 @@ module Make(S:S) = struct
       p >>= fun () ->
       c.is_syncing <- false;
       return () 
+
+  (* perform updates on lru; then maybe call sync, or else maybe_trim *)
+  let exec c ~sync:sync' ops = 
+    (* FIXME what is c.is_syncing? it's fine - we may call sync which
+       will wait till current sync completes *)
+    begin
+      ops |> List.iter (function
+          | `Insert (k,v) -> Lru.add k (Plain (insert v)) c.lru
+          | `Delete k -> Lru.add k (Plain (delete ())) c.lru)
+    end;
+    match sync' with
+    | false -> maybe_trim c
+    | true -> sync c
+
+  let rec find_opt c k = 
+    (* try to find in cache *)
+    Lru.find k c.lru |> function
+    | None -> begin
+        (* not in cache, so check whether we are syncing *)
+        match c.is_syncing with
+        | true -> 
+          c.syncing >>= fun () -> 
+          find_opt c k (* try again after sync completes *)
+        | false -> 
+          (* create the promise *)
+          let p = 
+            bot_ops.find_opt bot k >>= fun v -> 
+            begin match v with 
+            | None -> 
+              Lru.add k (Plain (lower_none ())) c.lru
+            | Some v -> 
+              Lru.add k (Plain (lower_some v)) c.lru
+            end;
+            maybe_trim c >>= fun () -> 
+            return v
+          in
+          Lru.add k (Finding p) c.lru;          
+          p                   
+      end
+    | Some (Plain (v,_)) -> return v
+    | Some (Finding p) -> p
       
-  let exec c ~sync ops = return () (* FIXME *)
-      (* perform updates on lru; then call sync; then maybe_trim *)
+  let insert c k v = 
+    Lru.add k (Plain (insert v)) c.lru;
+    maybe_trim c
+
+  let delete c k = 
+    Lru.add k (Plain (delete ())) c.lru;
+    maybe_trim c
 
   let ops = Top.{ find_opt; insert; delete; sync; exec }
   
